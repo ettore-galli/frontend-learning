bf1cbfc288d3005a7e3c7a6949e64b2337727d22
Ettore Galli <ettoregalli@MacBook-Pro-di-Ettore-2023.local>
2023-02-08 22:23:15 +0100

    Draft dashnboard

--- esempi/dashboard/src/index.js
+++ esempi/dashboard/src/index.js
@@ -1,25 +1,29 @@
 //import i18nStrings from '../assets/i18n/it/strings.json';
 
-((doc) => {
 
-    const i18nStrings = {
-        "page-title": "Esempio di dashboard"
-    };
+const i18nStrings = {
+    "page-title": "Esempio di dashboard"
+};
 
-    const applyI18nStrings = (i18nClass, i18nStrings) => {
-        Array.from(doc.getElementsByClassName(i18nClass).forEach(e => {
+const applyI18nStrings = (i18nClass, i18nStrings) => {
+    console.log("Apply i18n...")
+    Array.from(document.getElementsByClassName(i18nClass)).forEach(
+        e => {
             if (e.id) {
                 console.log(e.innerHTML, i18nStrings[e.id])
                 e.innerHTML = i18nStrings[e.id];
             }
-        });
-    }
+        }
+    )
+}
 
 
-    const startup = () => {
-        applyI18nStrings("i18n", i18nStrings);
-    }
+const startup = () => {
+    applyI18nStrings("i18n", i18nStrings);
+}
 
-    startup();
-})(document);
+startup();
+
+
+
 
