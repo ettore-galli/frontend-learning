9180e965bec3941580aeccdc05a719c8d28448c5
Ettore Galli <ettoregalli@MacBook-Pro-di-Ettore-2023.local>
2023-02-18 13:27:31 +0100

    Encapsulate state logic in a class

--- esempi/dashboard/src/app-state.js
+++ esempi/dashboard/src/app-state.js
@@ -1,25 +1,38 @@
 const APP_STATE_UPDATE_EVENT = "app-state-update";
 
-let centralAppState = {};
-const centralAppStateEventHub = new EventTarget();
 
-const triggerAppStateUpdateEvent = () => {
-    centralAppStateEventHub.dispatchEvent(new CustomEvent(APP_STATE_UPDATE_EVENT, { detail: centralAppState }));
+class ApplicationState {
+
+    constructor() {
+        this.centralAppState = {};
+        this.centralAppStateEventHub = new EventTarget();
+    }
+
+    triggerAppStateUpdateEvent() {
+        this.centralAppStateEventHub.dispatchEvent(new CustomEvent(APP_STATE_UPDATE_EVENT, { detail: this.centralAppState }));
+    }
+
+    setState(key, value) {
+        this.centralAppState = { ...this.centralAppState, [key]: value };
+        this.triggerAppStateUpdateEvent();
+    }
+
+    mergeWithState(key, part) {
+        this.centralAppState = { ...this.centralAppState, [key]: { ...this.centralAppState[key], ...part } };
+        console.log("post")
+        this.triggerAppStateUpdateEvent();
+    }
+
+    registerStateChangeListener(listener) {
+        return this.centralAppStateEventHub.addEventListener(APP_STATE_UPDATE_EVENT, (e) => { listener(e.detail) }, false);
+    }
+
 }
 
-const setState = (key, value) => {
-    centralAppState = { ...centralAppState, [key]: value };
-    triggerAppStateUpdateEvent();
-}
+const appState = new ApplicationState();
 
-const mergeWithState = (key, part) => {
-    centralAppState = { ...centralAppState, [key]: { ...centralAppState[key], ...part } };
-    triggerAppStateUpdateEvent();
-}
-
-const registerStateChangeListener = (listener) => {
-    return centralAppStateEventHub.addEventListener(APP_STATE_UPDATE_EVENT, (e) => { listener(e.detail) }, false);
-}
-
+const setState = (key, value) => { appState.setState(key, value); }
+const mergeWithState = (key, part) => { appState.mergeWithState(key, part); }
+const registerStateChangeListener = (listener) => { appState.registerStateChangeListener(listener); }
 
 export { setState, mergeWithState, registerStateChangeListener };
