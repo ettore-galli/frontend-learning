745eb351964e1b92860ab048b820429cc2a325c8
Ettore Galli <ettore.galli@deustechnology.com>
2023-05-14 14:42:01 +0200

    fix test

--- jest-testing/jest-testing/src/App.test.tsx
+++ jest-testing/jest-testing/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('test a element', () => {
   render(<App />);
   const linkElement = screen.getByText(/Selettore/i);
   expect(linkElement).toBeInTheDocument();
