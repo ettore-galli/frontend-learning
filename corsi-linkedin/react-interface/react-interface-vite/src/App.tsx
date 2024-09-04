498ce3dfb2cf9b7e1f67268a1dc91cd60fc415c8
Ettore Galli <ettore.galli@eng.it>
2024-09-04 21:52:11 +0200

    search

--- corsi-linkedin/react-interface/react-interface-vite/src/App.tsx
+++ corsi-linkedin/react-interface/react-interface-vite/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
-
 import { RiArchive2Line } from "react-icons/ri";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className='text-5xl'>
-        <RiArchive2Line className='inline-block text-red-400 align-top'/> Your appointments
-        </h1>
+        <RiArchive2Line className='inline-block text-red-400 align-top' /> Your appointments
+      </h1>
+      <Search />
     </div>
   );
 }
