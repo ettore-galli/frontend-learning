51314fe989dd2390dd815540885444154470ef9d
Ettore Galli <ettore.galli@eng.it>
2025-07-14 21:17:29 +0200

    Cleanup

--- corsi-linkedin/react-interface/react-interface-vite/src/utils/RandomId.ts
+++
@@ -1,8 +0,0 @@
-const getRandomId = () => {
-    const getIntRandom = (): number => {
-        return Math.floor(1000000 * Math.random());
-    }
-    return `${String(getIntRandom)}-${String(getIntRandom)}-${String(getIntRandom)}-${String(getIntRandom)}`;
-}
-
-export default getRandomId;
