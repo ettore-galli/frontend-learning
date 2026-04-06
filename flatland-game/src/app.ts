93f468c9a7450142ad3b6d7aa5bce6ef44f3bf94
ettore-galli <ettore.galli@gmail.com>
2026-04-06 20:11:41 +0200

    try a change

--- flatland-game/src/app.ts
+++ flatland-game/src/app.ts
@@ -1,6 +1,6 @@
 export function renderApp(root: HTMLElement) {
     root.innerHTML = `
-    <h1>Hello SPA</h1>
+    <h1>Hello SPA!</h1>
     <p>This is a minimalistic single page application.</p>
   `;
 }
