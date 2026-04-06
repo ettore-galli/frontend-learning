3e044c3cb0172fc350d630dc0fe6c60009214cd7
ettore-galli <ettore.galli@gmail.com>
2026-04-06 13:49:24 +0200

    Restore to a working point

--- flatland-game/dist/tests/example.test.js
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import { sum } from "../src/index.js";
-test("sum works", () => {
-    expect(sum(1, 2)).toBe(3);
-});
-//# sourceMappingURL=example.test.js.map
--- flatland-game/tests/functions.test.ts
+++ flatland-game/tests/functions.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { sum } from "../src/index.js";
+import { sum } from "../src/functions.js";
 
 test("sum works", () => {
   expect(sum(1, 2)).toBe(3);
